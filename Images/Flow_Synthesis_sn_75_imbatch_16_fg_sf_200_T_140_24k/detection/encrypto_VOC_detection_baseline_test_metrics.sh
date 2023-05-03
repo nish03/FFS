c8a8a0211541b4ac4d918594a67dcefa99970709
Nishant Kumar <nishant.kumar@tu-dresden.de>
2023-05-03 17:01:48 +0200

    First Commit

---
+++ Images/Flow_Synthesis_sn_75_imbatch_16_fg_sf_200_T_140_24k/detection/encrypto_VOC_detection_baseline_test_metrics.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+#SBATCH --time=00:30:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH --mem=5G  
+#SBATCH --partition=alpha
+module load modenv/hiera GCC/10.2.0 CUDA/11.1.1 OpenMPI/4.0.5 Python/3.8.6 
+source ~/python-environments/torchvision_env/bin/activate
+python voc_coco_plot.py  --name regnetx --thres 0.5959 --energy 1 --T 1 --seed 0
+ 
