0a480c4744d153ff8c4b486169854b494c5b0c03
Nishant Kumar <nishant.kumar@tu-dresden.de>
2023-05-03 17:18:33 +0200

    changed folder hierarchy

--- Video/Flow_synthesis_VIS_imbatch_16_fg_sf_200_T_11/detection/src/modeling/utils/__init__.py
+++
@@ -1,1 +0,0 @@
-from .image_list import ImageList, crop_tensor
--- Video/Flow_synthesis_VIS_imbatch_16_fg_sf_200_T_11/detection/src/modeling/vit/__init__.py
+++
@@ -1,3 +0,0 @@
-from .config import add_vit_config
-from .msvit import build_msvit_backbone
-from .dataset_mapper import FixSizeDatasetMapper
